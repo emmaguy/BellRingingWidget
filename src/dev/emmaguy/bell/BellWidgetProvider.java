db3ebd12e3518586f3e595f191c7005d8c5b2f8a
Emma Guy <emmag87@gmail.com>
2013-05-08 13:43:27 +0100

    minor tweaks

--- src/dev/emmaguy/bell/BellWidgetProvider.java
+++ src/dev/emmaguy/bell/BellWidgetProvider.java
@@ -14,7 +14,7 @@ public class BellWidgetProvider extends AppWidgetProvider
 {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
-    {
+    {   	
         for(int appWidgetId : appWidgetIds)
         {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.view);
